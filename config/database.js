80d147e8e3b77a0b8cc0aa23868cd0ca0843e6ae
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-10 11:42:06 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = {
 	| MongoDB URL
 	|--------------------------------------------------------------------------
 	*/
-	url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_inspeksi?authSource=admin'
+	url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_inspeksi?authSource=admin',
 	//url: 'mongodb://udb_inspeksi:tapinspeksiprod@dbapp.tap-agri.com:27017/s_inspeksi?authSource=admin',
 	ssl: true
 }

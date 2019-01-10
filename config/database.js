b6a2232f1fa8ea1b09f2b94cb8f8b4fc9906f792
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-10 14:53:58 +0700

    Ferdinand

--- config/database.js
+++ config/database.js
@@ -1,10 +1,55 @@
-module.exports = {
-	/*
-	|--------------------------------------------------------------------------
-	| MongoDB URL
-	|--------------------------------------------------------------------------
-	*/
-	url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_inspeksi?authSource=admin',
-	//url: 'mongodb://udb_inspeksi:tapinspeksiprod@dbapp.tap-agri.com:27017/s_inspeksi?authSource=admin',
-	ssl: true
-}
+/*
+|--------------------------------------------------------------------------
+| App Setup
+|--------------------------------------------------------------------------
+|
+| Untuk menghandle models, libraries, helper, node modules, dan lain-lain
+|
+*/
+	const fServer = require( 'fs' );
+	var set_db_config = [];
+	var connection_name = 'msa_ins_inspeksi';
+	var connection = {
+		host: '',
+		db_host: '',
+		username: '',
+		password: '',
+		port: '',
+		database: '',
+		auth_source: '',
+		ssl: false,
+	}
+
+	if ( fServer.existsSync( '/database-config.json' ) ) {
+		var rawdata = fServer.readFileSync( '/database-config.json' );
+		set_db_config.push( JSON.parse( rawdata ) );
+	}
+
+	if ( set_db_config.length > 0 ) {
+
+		var config = set_db_config[0];
+		var setup = config['setup'][connection_name];
+		var data = config['connections'][setup];
+
+		connection.host = data.host;
+		connection.username = data.username;
+		connection.password = data.password;
+		connection.port = data.port;
+		connection.database = data.database;
+		connection.auth_source = data.auth_source;
+		connection.ssl = data.ssl;
+	}
+
+/*
+|--------------------------------------------------------------------------
+| Module Exports
+|--------------------------------------------------------------------------
+*/
+	module.exports = {
+		//url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_inspeksi?authSource=admin',
+		//url: 'mongodb://udb_inspeksi:tapinspeksiprod@dbapp.tap-agri.com:27017/s_inspeksi?authSource=admin',
+		//ssl: true
+
+		url: 'mongodb://' + connection.username + ':' + connection.password + '@' + connection.host + ':' + connection.port + '/' + connection.database + '?authSource=' + connection.auth_source,
+		ssl: connection.ssl,
+	}

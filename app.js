0d6c1258653d2afd2ea5fd88ac7f4b52c4483887
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-10 10:52:08 +0700

    Ferdinand

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ mongoose.Promise = global.Promise;
 // Connecting to the database
 mongoose.connect( dbConfig.url, {
 	useNewUrlParser: true,
-	ssl: true
+	ssl: dbConfig.ssl
 } ).then( () => {
 	console.log( 'Successfully connected to the Database' );
 } ).catch( err => {
--- config/database.js
+++ config/database.js
@@ -1,3 +1,10 @@
 module.exports = {
-	url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_inspeksi?authSource=admin'
+	/*
+	|--------------------------------------------------------------------------
+	| MongoDB URL
+	|--------------------------------------------------------------------------
+	*/
+	//url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_inspeksi?authSource=admin'
+	url: 'mongodb://udb_inspeksi:tapinspeksiprod@dbapp.tap-agri.com:27017/s_inspeksi?authSource=admin',
+	ssl: false
 }

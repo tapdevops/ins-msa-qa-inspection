5e287e9793f7928b093cc8209ae177e989c18ee1
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-03-20 11:04:28 +0700

    Ferdinand

--- app/controllers/InspectionTrackingController.js
+++ app/controllers/InspectionTrackingController.js
@@ -30,7 +30,8 @@
 			{ "name": "LAT_TRACK", "value": parseFloat( req.body.LAT_TRACK ), "rules": "required|latitude" },
 			{ "name": "LONG_TRACK", "value": parseFloat( req.body.LONG_TRACK ), "rules": "required|longitude" },
 			{ "name": "INSERT_USER", "value": req.body.INSERT_USER, "rules": "required|alpha_numeric" },
-			{ "name": "INSERT_TIME", "value": req.body.INSERT_TIME.toString(), "rules": "required|exact_length(14)|numeric" }
+			{ "name": "INSERT_TIME", "value": req.body.INSERT_TIME.toString(), "rules": "required|exact_length(14)|numeric" },
+			{ "name": "STATUS_TRACK", "value": req.body.STATUS_TRACK.toString(), "rules": "required|numeric" }
 		];
 
 		var run_validator = validator.run( rules );
@@ -57,7 +58,8 @@
 				UPDATE_USER: "",
 				UPDATE_TIME: 0,
 				DELETE_USER: "",
-				DELETE_TIME: 0
+				DELETE_TIME: 0,
+				STATUS_TRACK: req.body.STATUS_TRACK
 			} );
 
 			set.save()
--- app/models/InspectionTrackingModel.js
+++ app/models/InspectionTrackingModel.js
@@ -24,6 +24,15 @@ const InspectionTrackingSchema = mongoose.Schema( {
 			return 0;
 		}
 	},
+	STATUS_TRACK: {
+		type: Number,
+		get: v => Math.floor( v ),
+		set: v => Math.floor( v ),
+		alias: 'i',
+		default: function() {
+			return 0;
+		}
+	},
 	INSERT_USER: String,
 	INSERT_TIME: {
 		type: Number,
